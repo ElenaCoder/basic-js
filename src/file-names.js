8890539a83c149beae4154d60242076e40db959d
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-02-12 17:33:09 +0200

    feat: add 10/21 solutions

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const countMap = {};
+
+  function getCountedName(name) {
+    if (countMap[name] === undefined) {
+      countMap[name] = 0;
+      return name;
+    }
+
+    countMap[name]++;
+    const newName = `${name}(${countMap[name]})`;
+    countMap[newName] = 0;
+    return newName;
+  }
+
+  return names.map((name) => getCountedName(name));
 }
 
 module.exports = {
